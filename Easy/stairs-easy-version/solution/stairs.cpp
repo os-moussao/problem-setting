1d8ebe2acf20ee99f80ba7872139f7939ed7aadb
Oussama Moussaoui <omoussaoui1337@gmail.com>
2023-06-23 23:36:36 +0100

    Add stairs (Easy Version)

---
+++ Easy/stairs-easy-version/solution/stairs.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int32_t main() {
+	int n; cin >> n;
+	int lo = 1, hi = 1e9, ans;
+	while (lo <= hi) {
+		int mid = (lo+hi)/2;
+		if ((mid*(mid+1))/2 <= n) {
+			ans=mid, lo=mid+1;
+		} else {
+			hi=mid-1;
+		}
+	}
+	cout << ans;
+}
